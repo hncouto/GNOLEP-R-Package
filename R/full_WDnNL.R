ef4f683fd669aca64c87bb34d034b99ed61b544d
hncouto <henriquenunocouto@gmail.com>
2026-06-07 15:44:01 +0100

    full_WDnNL
    Create a function to call the full database with all tables as present in the sqlite database.

---
+++ R/full_WDnNL.R
@@ -0,0 +1,14 @@
+#' Full WDnNL
+#'
+#' @param
+#'
+#' @returns Import all databse tables at once.
+#' @export
+
+full_WDnNL <- function() {
+  data(NativesWDnNL)
+  data(RealmsWDnNL)
+  data(RecordsWDnNL)
+  data(ReferencesWDnNL)
+  data(RegionsWDnNL)
+  data(TaxonomyWDnNL)}
