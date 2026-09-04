dec56a4064570e85674e7373f4a77a6835b1bf5c
hncouto <126601218+hncouto@users.noreply.github.com>
2026-09-04 12:09:26 +0100

    Update data.R

--- R/data.R
+++ R/data.R
@@ -7,17 +7,17 @@
 #' realms and references. This dataset is produced directly from the GNOLEP SQLite database
 #' (see `data-raw/data-preparation.R`).
 #'
-#' @format A data frame with 11916 records and 18 variables:
+#' @format A data frame with 10001 records and 18 variables:
 #'
-#' Database Variables (1): RecordID;
+#' Database Variables (1): GNOLEP:recordID;
 #'
-#' Introduction Variables (7): Cryptogenic, Intentional Release, Introduced, Dispersal, Established, Eradicated, First Record Year;
+#' Introduction Variables (7): GNOLEP:cryptogenic; GNOLEP:intentionalRelease; GNOLEP:introduced; GNOLEP:dispersal; GNOLEP:established; GNOLEP:eradicated; dwc:year - First Record Year;
 #'
-#' Taxonomic variables (3): Species as in the Original Record, Species as Accepted in GBIF, Genus, Family;
+#' Taxonomic variables (3): dwc:scientificName - Species as in the Original Record; GNOLEP:acceptedSpecies - Species as Accepted in GBIF; dwc:genus; dwc:family;
 #'
-#' Distribution Variables (4): Region, Country, Continent, Biogeographic Realm;
+#' Distribution Variables (4): dwc.verbatimLocality - Region; dwc.country; dwc.continent; GNOLEP:realm - Biogeographic Realm;
 #'
-#' Reference Data (2): Reference Citation, Reference Year;
+#' Reference Data (2): dwc:associatedReferences - Reference Citation: GNOLEP.referenceYear;
 #'
 #' \describe{
 #'   \item{GNOLEP.recordID}{integer; unique record id}
@@ -45,13 +45,13 @@
 #' Condensed GNOLEP
 #'
 #' Condensed Database to one Species per Region.
-#' @format A data frame with 3857 entries and a total of 13 variables.
+#' @format A data frame with 3986 entries and a total of 13 variables.
 #'
-#' Taxonomic variables (1): Accepted Species according to GBIF;
+#' Taxonomic variables (1): dwc:scientificName - Accepted Species according to GBIF;
 #'
-#' Distribution Variables (3): Region, Continent, Realm;
+#' Distribution Variables (3): dwc:verbatimLocality - Region; dwc:continent; GNOLEP:realm - Biogeographic Realm;
 #'
-#' Introduction Variables (7): Cryptogenic, Intentional Release, Introduced, Dispersal, Established, Eradicated, First Record Year;
+#' Introduction Variables (7): GNOLEP:cryptogenic; GNOLEP:intentionalRelease; GNOLEP:introduced; GNOLEP:dispersal; GNOLEP:established; GNOLEP:eradicated; dwc:year - First Record Year;
 #'
 #' Reference Data (2): Latest Reference (in the Complete database), Latest Reference Year
 #'
@@ -89,23 +89,62 @@
 "TaxonomyGNOLEP"
 
 #' Regions table
-#' @format A data frame...
+#' @format A data frame with the Regions table from GNOLEP
+#'\describe{
+#'   \item{GNOLEP.areaID}{character; region unique identifier}
+#'   \item{dwc.verbatimLocality}{character; Region name}
+#'   \item{dwc.country}{character; Country name}
+#'   \item{dwc.continent}{character; Continent name}
+#'}
+#'
 "RegionsGNOLEP"
 
 #' Realms table
-#' @format A data frame...
+#' @format A data frame with the Realms table from GNOLEP
+#'\describe{
+#'   \item{GNOLEP.realm}{character; Biogeographic Realm}
+#'   \item{GNOLEP.realmID}{character; Realm unique identifier}
+#'}
+#'
 "RealmsGNOLEP"
 
 #' References table
-#' @format A data frame...
+#' @format A data frame with the References table from GNOLEP
+#'\describe{
+#'   \item{GNOLEP.referenceID}{character; Bibliographic reference unique identifier}
+#'   \item{dwc.associatedReferences}{character; Bibliographic reference}
+#'   \item{GNOLEP.referenceYear}{integer; Bibliographic reference year}
+#'}
+#'
 "ReferencesGNOLEP"
 
 #' Records table
-#' @format A data frame...
+#' @format A data frame with the Records table from GNOLEP
+#'\describe{
+#'   \item{GNOLEP.speciesID}{character; species unique identifier}
+#'   \item{GNOLEP.areaID}{character; region unique identifier}
+#'   \item{GNOLEP.realmID}{character; Realm unique identifier}
+#'   \item{GNOLEP.cryptogenic}{binary; Cryptogenic Origin}
+#'   \item{GNOLEP.intentionalRelease}{binary; record of intentional introduction (biocontrol included)}
+#'   \item{GNOLEP.introduced}{binary; record of arrival related to human activity}
+#'   \item{GNOLEP.dispersal}{binary; Arrival by dispersal}
+#'   \item{GNOLEP.established}{binary; record of established population}
+#'   \item{GNOLEP.eradicated}{binary; Species previously established but currently eradicated}
+#'   \item{dwc.year}{integer; year of first record}
+#'   \item{GNOLEP.referenceID}{character; Bibliographic reference unique identifier}
+#'}
+#'
 "RecordsGNOLEP"
 
 #' Native Distribution Table
 #'
 #' Table with Native Distribution per Species
-#' @format A data frame with
+#' @format A data frame with the Native Conbtinent of established species in GNOLEP
+#'\describe{
+#'   \item{GNOLEP.speciesID}{character; species unique identifier}
+#'   \item{dwc.continent}{character; continent}
+#'   \item{GNOLEP.realmID}{character; Realm unique identifier}
+#'   \item{GNOLEP.referenceID}{character; Bibliographic reference unique identifier}
+#'}
+#'
 "NativesGNOLEP"
